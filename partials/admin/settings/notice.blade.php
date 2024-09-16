0717f74d76cd6f6e78399f9fb80d60bd40c621d2
navi <131613607+navi321AD@users.noreply.github.com>
2024-09-16 15:44:17 +0300

    Add files via upload

--- partials/admin/settings/notice.blade.php
+++ partials/admin/settings/notice.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="alert alert-danger">
-                    Your Panel is currently configured to read settings from the environment only. You will need to set <code>APP_ENVIRONMENT_ONLY=false</code> in your environment file in order to load settings dynamically.
+                    Ваша панель настроена на чтение настроек только из переменных окружения. Вам нужно установить <code>APP_ENVIRONMENT_ONLY=false</code> в вашем файле окружения, чтобы загружать настройки динамически.
                 </div>
             </div>
         </div>
